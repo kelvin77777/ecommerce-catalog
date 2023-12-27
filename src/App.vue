49615171641ad88d2231a27d19549e54eaeaa0f4
Kelvin Pahotton Simamora <kelvinsimamorakelvin@gmail.com>
2023-12-27 14:20:28 +0700

    Memasukkan project ke github

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductDisplay :product="currentProduct" @next-product="getNextProduct" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import ProductDisplay from "@/components/ProductDisplay.vue";
+
+const baseUrl = "https://fakestoreapi.com/products/";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProductDisplay,
+  },
+  data() {
+    return {
+      currentProductIndex: 1,
+      currentProduct: {},
+    };
+  },
+  methods: {
+    async getApi(index) {
+      try {
+        const response = await axios.get(baseUrl + index);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    },
+    async getNextProduct() {
+      if (this.currentProductIndex <= 20) {
+        const product = await this.getApi(this.currentProductIndex);
+        if (product) {
+          this.currentProduct = product;
+          this.currentProductIndex++;
+        }
+      }
+    },
+  },
+  mounted() {
+    this.getNextProduct();
+  },
+};
 </script>
 
 <style>
@@ -21,8 +52,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+button {
+  margin-top: 10px;
+  padding: 10px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,287 @@
+<template>
+    <div class="all" :class="sectionClass">
+      <div class="app-container" v-if="isMenOrWomenCategory">
+        <img
+          :src="product.image"
+          alt="Product Image"
+          style="
+            max-width: 200px;
+            max-height: 300px;
+            float: left;
+            margin-left: 100px;
+            margin-top: 0px;
+          "
+        />
+        <div style="margin-left: 100px; margin-top: 30px; margin-bottom: 1px">
+          <p class="Title"
+            style="
+              font-family: Arial, Helvetica, sans-serif;
+              font-size: x-large;
+              
+            "
+          >
+            {{ product.title }}
+          </p>
+          <p>
+            <span style="float: left">{{ product.category }}</span>
+            <span style="float: right">{{ product.rating.rate }}/5</span>
+          </p>
+          <br />
+          <hr />
+          <p
+            style="
+              font-family: 'Times New Roman', Times, serif;
+              font-size: larger;
+            "
+          >
+            {{ product.description }}
+          </p>
+          <hr />
+          <p class="Price"
+            style="
+              font-family: Arial, Helvetica, sans-serif;
+              font-size: x-large;
+              
+            "
+          >
+            ${{ product.price }}
+          </p>
+  
+          <button
+            class="Button1"
+            style="text-align: center; margin-top: 10px; margin-left: 0px"
+          >
+            Buy Now
+          </button>
+          <button
+            class="Button2"
+            @click="getNextProduct"
+            style="text-align: center; margin-top: 10px; margin-left: 40px"
+          >
+            Next Product
+          </button>
+        </div>
+      </div>
+      <div v-if="!isMenOrWomenCategory">
+        <img
+          :src="require('@/assets/image.png')"
+          alt=""
+          style="
+            padding: 70px;
+            width: 80%;
+            height: 30%;
+            margin: 0 auto;
+            display: block;
+          "
+        />
+        <p
+          style="
+            font-size: large;
+            text-align: center;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            margin-top: -28%;
+          "
+        >
+          This product is unavailable to show
+        </p>
+        <button
+          class="border-button3"
+          @click="getNextProduct"
+          style="
+            text-align: center;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            margin-top: -25%; /* Sesuaikan margin atas sesuai kebutuhan Anda */
+          "
+        >
+          Next Product
+        </button>
+      </div>
+      <div v-if="loading" class="loader-overlay">
+        <div class="loader"></div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      product: Object,
+    },
+    data() {
+      return {
+        loading: false,
+      };
+    },
+    computed: {
+      sectionClass() {
+        const category = this.product.category;
+        if (category === "men's clothing") {
+          return "men-section";
+        } else if (category === "women's clothing") {
+          return "women-section";
+        } else {
+          return "unavailable-product";
+        }
+      },
+      isMenOrWomenCategory() {
+        const category = this.product.category;
+        return category === "men's clothing" || category === "women's clothing";
+      },
+    },
+    methods: {
+      getNextProduct() {
+        // Show loader before loading data
+        this.loading = true;
+  
+        // Simulate API call (replace with your actual API call)
+        setTimeout(() => {
+          // Emit an event to ask the parent component to load the next product
+          this.$emit("next-product");
+  
+          // Hide loader after data is loaded
+          this.loading = false;
+        }, 1000); // Simulated API call time (2 seconds)
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .all {
+    background-color: var(--background-color);
+    height: 70vh;
+  }
+  
+  .app-container {
+    background-color: #ffffff;
+    padding: 20px;
+    border-radius: 10px;
+    width: 900px;
+    height: 600px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    margin-top: 20px;
+  }
+  
+  /* Men Section */
+  .men-section {
+    --background-color: #D6E6FF;
+  }
+  
+  .men-section .Title {
+    color: #002772;
+  }
+  
+  .men-section .Price{
+    color: #002772;
+  
+  }
+  
+  .men-section .Button1{
+    background-color: #002772;
+    color: #fff;
+    border: 2px solid #002772;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 230px;
+  }
+  
+  .men-section .Button2{
+    background-color: transparent;
+    color: #002772;
+    border: 2px solid #002772;
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 230px;
+  }
+  
+  /* Women Section */
+  .women-section {
+    --background-color: #FDE2FF;
+  }
+  
+  .women-section .Title{
+    color: #720060;
+  }
+  
+  .women-section .Price{
+    color: #720060;
+  }
+  
+  .women-section .Button1{
+    background-color: #720060;
+    color: #fff;
+    border: 2px solid #720060;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 230px;
+  }
+  
+  .women-section .Button2{
+    background-color: transparent;
+    color: #720060;
+    border: 2px solid #720060;
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 230px;
+  }
+  
+  /* Unavailable Product */
+  .unavailable-product {
+    --background-color: #DCDCDC;
+  }
+  
+  
+  
+  
+  
+  .border-button3 {
+    background-color: transparent;
+    color: #1e1e1e;
+    border: 2px solid #1e1e1e;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 400px;
+  }
+  
+  .loader-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+  }
+  
+  .loader {
+    border: 16px solid #f3f3f3;
+    border-top: 16px solid #3498db;
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+  }
+  
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  </style>
+  
